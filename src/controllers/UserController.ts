b1c4bf1b2edd571f15b5767801c3a95993b8047d
mkawsar <kawsar.diu888@gmail.com>
2023-06-15 14:23:40 +0600

    Error handling

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,16 +6,21 @@ import passport from 'passport';
 import '../middlewares/PassportHandler';
 import { User } from '../models/User';
 import { JWT_SECRET } from '../utils/Secrets';
+import { getErrorMessage } from '../utils/Errors';
 
 export class UserController {
     public async registerUser(req: Request, res: Response): Promise<void> {
-        const hashedPassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-        await User.create({
-            username: req.body.username,
-            password: hashedPassword
-        });
+        try {
+            const hashedPassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+            await User.create({
+                username: req.body.username,
+                password: hashedPassword
+            });
 
-        const token = jwt.sign({ username: req.body.username, scope: req.body.scope }, JWT_SECRET);
-        res.status(200).send({ token: token });
+            //const token = jwt.sign({ username: req.body.username, scope: req.body.scope }, JWT_SECRET);
+            res.status(200).send({ message: `username ${req.body.username} created successfully!` });
+        } catch (err) {
+            res.status(500).send(getErrorMessage(err));
+        }
     }
 }
